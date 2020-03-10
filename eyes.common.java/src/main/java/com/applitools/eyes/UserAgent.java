fed4ca40dddd39a2cc58f372eaa36c36d35653b4
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-03-10 15:57:19 +0200

    Follow Java casing.

--- eyes.common.java/src/main/java/com/applitools/eyes/UserAgent.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/UserAgent.java
@@ -11,29 +11,29 @@ import java.util.regex.Pattern;
  * Handles parsing of a user agent string
  */
 public class UserAgent {
-    private static final String MajorMinor = "(?<major>\\d+)(?:[_.](?<minor>\\d+))?";
-    private static final String Product = "(?:(?<product>%s)/" + MajorMinor + ")";
+    private static final String MAJOR_MINOR = "(?<major>\\d+)(?:[_.](?<minor>\\d+))?";
+    private static final String PRODUCT = "(?:(?<product>%s)/" + MAJOR_MINOR + ")";
 
-    private static final Pattern VerRegex = Pattern.compile(String.format(Product, "Version"));
+    private static final Pattern VERSION_REGEX = Pattern.compile(String.format(PRODUCT, "Version"));
 
-    private static final Pattern[] BrowserRegex = new Pattern[]{
-            Pattern.compile(String.format(Product, "Opera")),
-            Pattern.compile(String.format(Product, "Chrome")),
-            Pattern.compile(String.format(Product, "Safari")),
-            Pattern.compile(String.format(Product, "Firefox")),
-            Pattern.compile(String.format(Product, "Edge")),
-            Pattern.compile("(?:MS(?<product>IE) " + MajorMinor + ")")
+    private static final Pattern[] BROWSER_REGEX = new Pattern[]{
+            Pattern.compile(String.format(PRODUCT, "Opera")),
+            Pattern.compile(String.format(PRODUCT, "Chrome")),
+            Pattern.compile(String.format(PRODUCT, "Safari")),
+            Pattern.compile(String.format(PRODUCT, "Firefox")),
+            Pattern.compile(String.format(PRODUCT, "Edge")),
+            Pattern.compile("(?:MS(?<product>IE) " + MAJOR_MINOR + ")")
     };
 
-    private static final Pattern[] OSRegex = new Pattern[]{
-            Pattern.compile("(?:(?<os>Windows NT) " + MajorMinor + ")"),
+    private static final Pattern[] OS_REGEX = new Pattern[]{
+            Pattern.compile("(?:(?<os>Windows NT) " + MAJOR_MINOR + ")"),
             Pattern.compile("(?:(?<os>Windows XP))"),
             Pattern.compile("(?:(?<os>Windows 2000))"),
             Pattern.compile("(?:(?<os>Windows NT))"),
             Pattern.compile("(?:(?<os>Windows))"),
-            Pattern.compile("(?:(?<os>Mac OS X) " + MajorMinor + ")"),
-            Pattern.compile("(?:(?<os>Android) " + MajorMinor + ")"),
-            Pattern.compile("(?:(?<os>CPU(?: i[a-zA-Z]+)? OS) " + MajorMinor + ")"),
+            Pattern.compile("(?:(?<os>Mac OS X) " + MAJOR_MINOR + ")"),
+            Pattern.compile("(?:(?<os>Android) " + MAJOR_MINOR + ")"),
+            Pattern.compile("(?:(?<os>CPU(?: i[a-zA-Z]+)? OS) " + MAJOR_MINOR + ")"),
             Pattern.compile("(?:(?<os>Mac OS X))"),
             Pattern.compile("(?:(?<os>Mac_PowerPC))"),
             Pattern.compile("(?:(?<os>Linux))"),
@@ -41,45 +41,45 @@ public class UserAgent {
             Pattern.compile("(?:(?<os>SymbOS))")
     };
 
-    private static final Pattern HiddenIE = Pattern.compile(
-            "(?:(?:rv:" + MajorMinor + "\\) like Gecko))");
+    private static final Pattern HIDDEN_IE_REGEX = Pattern.compile(
+            "(?:(?:rv:" + MAJOR_MINOR + "\\) like Gecko))");
 
-    private static final Pattern Edge = Pattern.compile(String.format(Product, "Edge"));
+    private static final Pattern EDGE_REGEX = Pattern.compile(String.format(PRODUCT, "Edge"));
 
     private String originalUserAgentString;
-    private String OS;
-    private String OSMajorVersion;
-    private String OSMinorVersion;
-    private String Browser;
-    private String BrowserMajorVersion;
-    private String BrowserMinorVersion;
+    private String os;
+    private String osMajorVersion;
+    private String osMinorVersion;
+    private String browser;
+    private String browserMajorVersion;
+    private String browserMinorVersion;
 
     public String getOriginalUserAgentString() {
         return originalUserAgentString;
     }
 
     public String getOS() {
-        return OS;
+        return os;
     }
 
     public String getOSMajorVersion() {
-        return OSMajorVersion;
+        return osMajorVersion;
     }
 
     public String getOSMinorVersion() {
-        return OSMinorVersion;
+        return osMinorVersion;
     }
 
     public String getBrowser() {
-        return Browser;
+        return browser;
     }
 
     public String getBrowserMajorVersion() {
-        return BrowserMajorVersion;
+        return browserMajorVersion;
     }
 
     public String getBrowserMinorVersion() {
-        return BrowserMinorVersion;
+        return browserMinorVersion;
     }
 
     /**
@@ -102,11 +102,11 @@ public class UserAgent {
         UserAgent result = new UserAgent();
         result.originalUserAgentString = userAgent;
 
-        // OS
+        // os
         Map<String, Matcher> oss = new HashMap<>();
         List<Matcher> matchers = new ArrayList<>();
 
-        for (Pattern osRegex : OSRegex) {
+        for (Pattern osRegex : OS_REGEX) {
             Matcher matcher = osRegex.matcher(userAgent);
             if (matcher.find()) {
                 matchers.add(matcher);
@@ -124,9 +124,9 @@ public class UserAgent {
         Matcher osMatch;
         if (matchers.size() == 0) {
             if (unknowns) {
-                result.OS = OSNames.Unknown;
+                result.os = OSNames.Unknown;
             } else {
-                throw new NotSupportedException("Unknown OS: " + userAgent);
+                throw new NotSupportedException("Unknown os: " + userAgent);
             }
         } else {
             if (oss.size() > 1 && oss.containsKey("android")) {
@@ -135,81 +135,81 @@ public class UserAgent {
                 osMatch = oss.values().toArray(new Matcher[0])[0];
             }
 
-            result.OS = osMatch.group("os");
+            result.os = osMatch.group("os");
             if (osMatch.groupCount() > 1) {
-                result.OSMajorVersion = osMatch.group("major");
+                result.osMajorVersion = osMatch.group("major");
             }
-            if (result.OSMajorVersion == null) {
-                result.OSMajorVersion = "";
+            if (result.osMajorVersion == null) {
+                result.osMajorVersion = "";
             }
             if (osMatch.groupCount() > 2) {
-                result.OSMinorVersion = osMatch.group("minor");
-                if (result.OSMinorVersion == null || result.OSMinorVersion.length() == 0) {
-                    result.OSMinorVersion = "0";
+                result.osMinorVersion = osMatch.group("minor");
+                if (result.osMinorVersion == null || result.osMinorVersion.length() == 0) {
+                    result.osMinorVersion = "0";
                 }
             }
-            if (result.OSMinorVersion == null) {
-                result.OSMinorVersion = "";
+            if (result.osMinorVersion == null) {
+                result.osMinorVersion = "";
             }
         }
 
-        // OS Normalization
-        if (result.OS.toUpperCase().startsWith("CPU")) {
-            result.OS = OSNames.IOS;
-        } else if (result.OS.equals("Windows XP")) {
-            result.OS = OSNames.Windows;
-            result.OSMajorVersion = "5";
-            result.OSMinorVersion = "1";
-        } else if (result.OS.equals("Windows 2000")) {
-            result.OS = OSNames.Windows;
-            result.OSMajorVersion = "5";
-            result.OSMinorVersion = "0";
-        } else if (result.OS.equals("Windows NT")) {
-            result.OS = OSNames.Windows;
-            if (result.OSMajorVersion.equals("6") && result.OSMinorVersion.equals("1")) {
-                result.OSMajorVersion = "7";
-                result.OSMinorVersion = "0";
-            } else if (result.OSMajorVersion.trim().length() == 0) {
-                result.OSMajorVersion = "4";
-                result.OSMinorVersion = "0";
+        // os Normalization
+        if (result.os.toUpperCase().startsWith("CPU")) {
+            result.os = OSNames.IOS;
+        } else if (result.os.equals("Windows XP")) {
+            result.os = OSNames.Windows;
+            result.osMajorVersion = "5";
+            result.osMinorVersion = "1";
+        } else if (result.os.equals("Windows 2000")) {
+            result.os = OSNames.Windows;
+            result.osMajorVersion = "5";
+            result.osMinorVersion = "0";
+        } else if (result.os.equals("Windows NT")) {
+            result.os = OSNames.Windows;
+            if (result.osMajorVersion.equals("6") && result.osMinorVersion.equals("1")) {
+                result.osMajorVersion = "7";
+                result.osMinorVersion = "0";
+            } else if (result.osMajorVersion.trim().length() == 0) {
+                result.osMajorVersion = "4";
+                result.osMinorVersion = "0";
             }
-        } else if (result.OS.equals("Mac_PowerPC")) {
-            result.OS = OSNames.Macintosh;
-        } else if (result.OS.equals("CrOS")) {
-            result.OS = OSNames.ChromeOS;
+        } else if (result.os.equals("Mac_PowerPC")) {
+            result.os = OSNames.Macintosh;
+        } else if (result.os.equals("CrOS")) {
+            result.os = OSNames.ChromeOS;
         }
 
-        // Browser
+        // browser
         boolean browserOK = false;
 
-        for (Pattern browserRegex : BrowserRegex) {
+        for (Pattern browserRegex : BROWSER_REGEX) {
             Matcher matcher = browserRegex.matcher(userAgent);
             if (matcher.find()) {
-                result.Browser = matcher.group("product");
-                result.BrowserMajorVersion = matcher.group("major");
-                result.BrowserMinorVersion = matcher.group("minor");
+                result.browser = matcher.group("product");
+                result.browserMajorVersion = matcher.group("major");
+                result.browserMinorVersion = matcher.group("minor");
                 browserOK = true;
                 break;
             }
         }
 
-        if (result.OS.equals(OSNames.Windows)) {
-            Matcher edgeMatch = Edge.matcher(userAgent);
+        if (result.os.equals(OSNames.Windows)) {
+            Matcher edgeMatch = EDGE_REGEX.matcher(userAgent);
             if (edgeMatch.find()) {
-                result.Browser = BrowserNames.Edge;
-                result.BrowserMajorVersion = edgeMatch.group("major");
-                result.BrowserMinorVersion = edgeMatch.group("minor");
+                result.browser = BrowserNames.Edge;
+                result.browserMajorVersion = edgeMatch.group("major");
+                result.browserMinorVersion = edgeMatch.group("minor");
             }
 
             // IE11 and later is "hidden" on purpose.
             // http://blogs.msdn.com/b/ieinternals/archive/2013/09/21/
             //   internet-explorer-11-user-agent-string-ua-string-sniffing-
             //   compatibility-with-gecko-webkit.aspx
-            Matcher iematch = HiddenIE.matcher(userAgent);
+            Matcher iematch = HIDDEN_IE_REGEX.matcher(userAgent);
             if (iematch.find()) {
-                result.Browser = BrowserNames.IE;
-                result.BrowserMajorVersion = iematch.group("major");
-                result.BrowserMinorVersion = iematch.group("minor");
+                result.browser = BrowserNames.IE;
+                result.browserMajorVersion = iematch.group("major");
+                result.browserMinorVersion = iematch.group("minor");
 
                 browserOK = true;
             }
@@ -217,17 +217,17 @@ public class UserAgent {
 
         if (!browserOK) {
             if (unknowns) {
-                result.Browser = "Unknown";
+                result.browser = "Unknown";
             } else {
-                throw new NotSupportedException("Unknown Browser: " + userAgent);
+                throw new NotSupportedException("Unknown browser: " + userAgent);
             }
         }
 
-        // Explicit Browser version (if available)
-        Matcher versionMatch = VerRegex.matcher(userAgent);
+        // Explicit browser version (if available)
+        Matcher versionMatch = VERSION_REGEX.matcher(userAgent);
         if (versionMatch.find()) {
-            result.BrowserMajorVersion = versionMatch.group("major");
-            result.BrowserMinorVersion = versionMatch.group("minor");
+            result.browserMajorVersion = versionMatch.group("major");
+            result.browserMinorVersion = versionMatch.group("minor");
         }
 
         return result;
