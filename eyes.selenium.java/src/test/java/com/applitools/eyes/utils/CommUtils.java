2a1ee70890bf3d6898f98897c240328d14692040
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-03-12 16:21:23 +0200

    Forgotten file

--- eyes.selenium.java/src/test/java/com/applitools/eyes/utils/CommUtils.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/utils/CommUtils.java
@@ -1,4 +1,12 @@
 package com.applitools.eyes.utils;
 
-public class CommUtils {
+public class CommUtils extends CommunicationUtils{
+
+    public static void putTestResultJsonToSauceLabs(PassedResult passedResult, String sessionId) {
+        String sauce_username = System.getenv("SAUCE_USERNAME");
+        String sauce_access_key = System.getenv("SAUCE_ACCESS_KEY");
+        HttpAuth creds = HttpAuth.basic(sauce_username, sauce_access_key);
+        putJson("https://saucelabs.com/rest/v1/" + sauce_username + "/jobs/" + sessionId, passedResult, creds);
+    }
+
 }
