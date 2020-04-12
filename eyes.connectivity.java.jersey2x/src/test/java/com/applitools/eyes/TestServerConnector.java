a002a4fccc40019d0560ea2ba635d4c47e466b1b
Tal Goldrat <tal.goldrat@applitools.com>
2020-04-12 20:01:12 +0300

    Added unitests to jersey2 server connector

--- eyes.connectivity.java.jersey2x/src/test/java/com/applitools/eyes/TestServerConnector.java
+++ eyes.connectivity.java.jersey2x/src/test/java/com/applitools/eyes/TestServerConnector.java
@@ -1,10 +1,8 @@
 package com.applitools.eyes;
 
-import com.applitools.utils.GeneralUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -17,7 +15,6 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import javax.ws.rs.HttpMethod;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
@@ -26,7 +23,8 @@ import javax.ws.rs.core.Response;
 import java.net.URI;
 
 import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(RestClient.class)
@@ -39,42 +37,33 @@ public class TestServerConnector {
     Client restClient;
 
     @Mock
-    WebTarget endPoint;
+    WebTarget target;
+
+    @Mock
+    Invocation.Builder request;
+
+    @Mock
+    Response response;
 
     ObjectMapper jsonMapper = new ObjectMapper();
 
     RunningSession runningSession = new RunningSession();
 
-    // Below are the mocks for the long request
-    String getRequestUri = "getUri";
-    String deleteRequestUri = "deleteUri";
-
-    @Mock
-    WebTarget getRequestTarget;
-    @Mock
-    WebTarget deleteRequestTarget;
-
-    @Mock
-    Invocation.Builder firstRequest;
-    @Mock
-    Invocation.Builder getRequest;
-    @Mock
-    Invocation.Builder deleteRequest;
-
-    @Mock
-    Response firstResponse;
-    @Mock
-    Response getResponse;
-    @Mock
-    Response deleteResponse;
-
     @Before
     public void setMocks() {
         PowerMockito.mockStatic(RestClient.class);
         when(RestClient.buildRestClient(anyInt(), (AbstractProxySettings) any())).thenReturn(restClient);
-        when(restClient.target(GeneralUtils.getServerUrl())).thenReturn(endPoint);
-        when(endPoint.path(anyString())).thenReturn(endPoint);
-        when(endPoint.queryParam(anyString(), any())).thenReturn(endPoint);
+        when(restClient.target((URI) any())).thenReturn(target);
+        when(target.path(anyString())).thenReturn(target);
+        when(target.queryParam(anyString(), any())).thenReturn(target);
+        when(target.request(anyString())).thenReturn(request);
+        when(request.header(anyString(), any())).thenReturn(request);
+        when(request.method(anyString(), (Entity<?>) any())).thenReturn(response);
+        when(response.getHeaderString(anyString())).thenReturn(null);
+
+        Response.StatusType statusType = mock(Response.StatusType.class);
+        when(response.getStatusInfo()).thenReturn(statusType);
+        when(statusType.getReasonPhrase()).thenReturn("");
 
         jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         runningSession.setUrl("");
@@ -84,53 +73,8 @@ public class TestServerConnector {
         runningSession.setSessionId("");
     }
 
-    /**
-     * Mocking the process of long request
-     * @param httpMethod The method of the first request
-     * @return The last response from the long request
-     */
-    public Response mockLongRequest(String httpMethod) {
-        // Handle the third request in the long request - DELETE request
-        when(restClient.target(deleteRequestUri)).thenReturn(deleteRequestTarget);
-        when(deleteRequestTarget.queryParam(anyString(), any())).thenReturn(deleteRequestTarget);
-        when(deleteRequestTarget.request((String) null)).thenReturn(deleteRequest);
-        when(deleteRequest.method(HttpMethod.DELETE)).thenReturn(deleteResponse);
-
-        // Handle the second request in the long request - GET request
-        when(restClient.target(getRequestUri)).thenReturn(getRequestTarget);
-        when(getRequestTarget.queryParam(anyString(), any())).thenReturn(getRequestTarget);
-        when(getRequestTarget.request((String) null)).thenReturn(getRequest);
-        when(getRequest.method(HttpMethod.GET)).thenReturn(getResponse);
-        when(getResponse.getHeaderString(HttpHeaders.LOCATION)).thenReturn(deleteRequestUri);
-        when(getResponse.getStatus()).thenReturn(Response.Status.CREATED.getStatusCode());
-
-        // Handle the first request in the long request
-        when(endPoint.request(anyString())).thenReturn(firstRequest);
-        when(firstRequest.header(anyString(), any())).thenReturn(firstRequest);
-        when(firstRequest.method(eq(httpMethod), (Entity<?>) any())).thenReturn(firstResponse);
-        when(firstResponse.getHeaderString(HttpHeaders.LOCATION)).thenReturn(getRequestUri);
-        when(firstResponse.getStatus()).thenReturn(Response.Status.ACCEPTED.getStatusCode());
-
-        return deleteResponse;
-    }
-
-    public void verifyLongRequest() {
-        verify(restClient).target(getRequestUri);
-        verify(getRequestTarget).request((String) null);
-        verify(getRequest).method(HttpMethod.GET);
-
-        verify(restClient).target(deleteRequestUri);
-        verify(deleteRequestTarget).request((String) null);
-        verify(deleteRequest).method(HttpMethod.DELETE);
-    }
-
     @Test
     public void TestStartSessionGotIsNew() throws JsonProcessingException {
-        Response response = mockLongRequest(HttpMethod.POST);
-        Response.StatusType statusType = mock(Response.StatusType.class);
-        when(response.getStatusInfo()).thenReturn(statusType);
-        when(statusType.getReasonPhrase()).thenReturn("");
-
         ServerConnector connector = new ServerConnector();
         connector.setLogger(new Logger());
 
@@ -139,7 +83,6 @@ public class TestServerConnector {
         when(response.readEntity(String.class)).thenReturn(jsonMapper.writeValueAsString(runningSession));
         RunningSession session = connector.startSession(new SessionStartInfo());
         Assert.assertTrue(session.getIsNew());
-        verifyLongRequest();
 
         runningSession.setIsNew(false);
         when(response.readEntity(String.class)).thenReturn(jsonMapper.writeValueAsString(runningSession));
