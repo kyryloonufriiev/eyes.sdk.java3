bb6c39c68b76b6fb669c6b2c4fe00d47a0fab3bd
Daniel Puterman <daniel.puterman@applitools.com>
2020-04-25 16:48:52 +0300

    removed error trap from travis script

--- runTests.sh
+++ runTests.sh
@@ -12,13 +12,13 @@ dump_output() {
    echo Tailing the last 500 lines of output:
    tail -500 $BUILD_OUTPUT
 }
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  exit 1
-}
-# If an error occurs, run our error handler to output a tail of the build
-trap 'error_handler' ERR
+#error_handler() {
+#  echo ERROR: An error was encountered with the build.
+#  dump_output
+#  exit 1
+#}
+## If an error occurs, run our error handler to output a tail of the build
+#trap 'error_handler' ERR
 
 # Set up a repeating loop to send some output to Travis.
 
