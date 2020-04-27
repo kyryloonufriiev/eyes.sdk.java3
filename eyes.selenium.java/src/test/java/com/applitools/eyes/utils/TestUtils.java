736a5bebc752f8feffb4f940ee067e8ea96e935f
Itai Bar-Haim <itai.barhaim@applitools.com>
2020-04-27 14:06:19 +0300

    returned log verbosity to verbose.

--- eyes.selenium.java/src/test/java/com/applitools/eyes/utils/TestUtils.java
+++ eyes.selenium.java/src/test/java/com/applitools/eyes/utils/TestUtils.java
@@ -51,7 +51,7 @@ public class TestUtils {
 //            String path = logPath != null ? logPath : initLogPath(testName);
 //            return new FileLogger(path + File.separator + "log.log", false, true);
 //        }
-        return new StdoutLogHandler(false);
+        return new StdoutLogHandler(true);
     }
 
     public static LogHandler initLogger(String methodName) {
