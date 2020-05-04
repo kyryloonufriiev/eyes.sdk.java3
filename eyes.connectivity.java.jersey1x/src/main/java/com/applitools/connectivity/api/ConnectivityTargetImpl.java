c1f887c6f1544a86d155c9e22ca4622801c79a41
Tal Goldrat <tal.goldrat@applitools.com>
2020-05-04 20:05:56 +0300

    Fixed rebase

---
+++ eyes.connectivity.java.jersey1x/src/main/java/com/applitools/connectivity/api/ConnectivityTargetImpl.java
@@ -0,0 +1,43 @@
+package com.applitools.connectivity.api;
+
+import com.applitools.utils.ArgumentGuard;
+import com.sun.jersey.api.client.AsyncWebResource;
+import com.sun.jersey.api.client.WebResource;
+
+public class ConnectivityTargetImpl implements ConnectivityTarget {
+
+    WebResource target;
+    AsyncWebResource asyncTarget;
+
+    ConnectivityTargetImpl(WebResource target, AsyncWebResource asyncTarget) {
+        this.target = target;
+        this.asyncTarget = asyncTarget;
+    }
+
+    @Override
+    public ConnectivityTarget path(String path) {
+        ArgumentGuard.notNull(path, "path");
+        target = target.path(path);
+        asyncTarget = asyncTarget.path(path);
+        return this;
+    }
+
+    @Override
+    public ConnectivityTarget queryParam(String name, String value) {
+        ArgumentGuard.notNullOrEmpty(name, "name");
+        ArgumentGuard.notNullOrEmpty(value, "value");
+        target = target.queryParam(name, value);
+        asyncTarget = asyncTarget.queryParam(name, value);
+        return this;
+    }
+
+    @Override
+    public Request request(String... acceptableResponseTypes) {
+        return new RequestImpl(target.accept(acceptableResponseTypes));
+    }
+
+    @Override
+    public AsyncRequest asyncRequest(String... acceptableResponseTypes) {
+        return new AsyncRequestImpl(asyncTarget.accept(acceptableResponseTypes));
+    }
+}
