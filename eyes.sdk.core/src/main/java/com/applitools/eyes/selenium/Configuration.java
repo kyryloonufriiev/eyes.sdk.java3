b0f515fe9d64e857ab3c68616e931374ad13650e
talgoldy <62615450+talgoldy@users.noreply.github.com>
2020-06-25 16:30:42 +0300

    Bugfix trello 1954 adding config missing constructors

--- eyes.sdk.core/src/main/java/com/applitools/eyes/selenium/Configuration.java
+++ eyes.sdk.core/src/main/java/com/applitools/eyes/selenium/Configuration.java
@@ -1,7 +1,29 @@
 package com.applitools.eyes.selenium;
 
+import com.applitools.eyes.RectangleSize;
+
 /**
  * Soon to be deprecated. Please use {@link com.applitools.eyes.config.Configuration}
  */
 public class Configuration extends com.applitools.eyes.config.Configuration {
+
+    public Configuration() {
+        super();
+    }
+
+    public Configuration(com.applitools.eyes.config.Configuration configuration) {
+        super(configuration);
+    }
+
+    public Configuration(RectangleSize viewportSize) {
+        super(viewportSize);
+    }
+
+    public Configuration(String testName) {
+        super(testName);
+    }
+
+    public Configuration(String appName, String testName, RectangleSize viewportSize) {
+        super(appName, testName, viewportSize);
+    }
 }
