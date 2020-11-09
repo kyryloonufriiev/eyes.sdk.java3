f5df209d6ffd2baef7a931275a728da87124df2a
Daria Yerofieieva <64783589+DariaYerofieieva@users.noreply.github.com>
2020-11-09 16:03:27 +0200

    Rfe trello 2286 Assembling test results (#413)

---
+++ sendTestResults.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+send_test_results() {
+  jq -s '.[0].results = [.[].results | add] | .[0]' "$1"*.json |
+    curl -sS \
+      -X POST \
+      -H 'Content-Type: application/json' \
+      -d @- http://sdk-test-results.herokuapp.com/result
+}
+
+cd "$TRAVIS_BUILD_DIR/report" || exit
+
+groups=$(for file in *.json; do echo "${file%_*.json}"; done | sort -u)
+for group in $groups; do
+  send_test_results "$group"
+done
